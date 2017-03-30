e2abc5bdb3b29d501dbd2c74d29e26da1daa66d4
Chris <tw.chrislyu@gmail.com>
2017-03-30 13:56:56 +0800

    Add edit & destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,6 +23,23 @@ class ProjectsController < ApplicationController
   	
   end
 
+  def edit
+  	
+  end
+
+  def update
+  	if @project.update(project_params)
+  		redirect_to @project, notice: "That project was successfully updated"
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@project.destroy
+  	redirect_to projects_path
+  end
+
   private
 
   def project_params
