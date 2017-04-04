586e47f134502999b3636c6e3f84c2b828dc228c
Chris <tw.chrislyu@gmail.com>
2017-04-04 21:07:56 +0800

    add user and stock association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
 	def  my_portfolio
-		
+		@user_stocks = current_user.user_stocks
+		@user = current_user
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401142448) do
+ActiveRecord::Schema.define(version: 20170404002418) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20170401142448) do
     t.string   "ticker"
     t.string   "name"
     t.decimal  "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
