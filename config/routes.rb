8b854cb30643e04a60afcca977aed5f4be9bfa9e
Chris <tw.chrislyu@gmail.com>
2017-04-05 14:05:59 +0800

    my_friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
- 
+  resources :users, only: [:show]
+  resources :friendships
+  
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
